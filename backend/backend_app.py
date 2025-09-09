8b48c49ccd87c145c1ee161b7970cfdf9e67f4a7
Sandras Mac <sandra@mac.speedport.ip>
2025-09-09 23:58:38 +0200

    implementing update, search and sort options in the backend

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -14,7 +14,23 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     """Function to get the posts."""
-    return jsonify(POSTS)
+    sort = request.args.get('sort')
+    direction = request.args.get('direction')
+
+    accept_sort_para = {'title', 'content'}
+    accept_direction_para = {'asc', 'desc'}
+
+    if sort and direction:
+        if sort in accept_sort_para and direction in accept_direction_para:
+            reverse = True if direction == 'desc' else False
+            sorted_posts = sorted(POSTS, key=lambda post: post[sort], reverse=reverse)
+            return jsonify(sorted_posts)
+        else:
+            return jsonify({'error': 'Invalid sort or direction parameter'}), 400
+    elif not sort and not direction:
+        return jsonify(POSTS)
+    else:
+        return jsonify({'error': 'Boath sort and direction parameters are required.'}), 400
 
 
 @app.route('/api/posts', methods=['POST'])
@@ -38,6 +54,34 @@ def delete_post(post_id):
             POSTS.remove(post)
             return jsonify({'message': f'Post with id {post_id} has been deleted successfully.'}), 200
     return jsonify({'message': f'Post with id {post_id} was not found.'}), 404
+
+
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    """Function to update a post by ID."""
+    for post in POSTS:
+        if post['id'] == post_id:
+            post.update(request.get_json())
+            return jsonify(post), 200
+    return jsonify({'message': f'Post with id {post_id} was not found.'}), 404
+
+
+@app.route('/api/posts/search', methods=['GET'])
+def search_posts():
+    """Function to search posts by title."""
+    title = request.args.get('title', '').lower()
+    content = request.args.get('content', '').lower()
+    search_lst = []
+
+    for post in POSTS:
+        match_title =  title in post['title'].lower() if title else True
+        match_content = content in post['content'].lower() if content else True
+
+        if match_title and match_content:
+            search_lst.append(post)
+
+            
+    return jsonify(search_lst), 200
 
 
 
